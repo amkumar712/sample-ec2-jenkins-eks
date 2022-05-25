6413e7f233e299c315f488c83601da49bcaa6baf
Muthukumar Arumugam <amkumar712@gmail.com>
2022-05-24 21:10:45 -0400

    Update Controller - Adding new endpoint

--- src/main/java/com/ecs/eks/sample/controller/SampleController.java
+++ src/main/java/com/ecs/eks/sample/controller/SampleController.java
@@ -1,6 +1,7 @@
 package com.ecs.eks.sample.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,11 @@ public class SampleController {
     public String helloWorld() {
         System.out.println("This is in HelloWorld Method");
         return "Hello World!!!";
+    }
+
+    @GetMapping("/greetings")
+    public String greeting(@RequestHeader("name") String name) {
+        System.out.println("This is in Greetings Method");
+        return "Hello " + name + "!!!";
     }
 }
